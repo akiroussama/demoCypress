324ba6d2ba6dadd8b8a9a60e69b65e776ff5bb5a
oakir <oakir@ecomundo.eu>
2022-05-14 09:02:27 +0200

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import TodoDate from "./components/TodoDate";
 import ItemList from "./components/ItemList";
+import Header from "./components/Header";
 import { AppStateProvider } from "./AppContext";
 
 function App() {
   return (
     <AppStateProvider>
+      <Header />
       <TodoDate />
       <ItemList />
     </AppStateProvider>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Header.module.scss";
+
+// Current date at the top of the page
+function Header() {
+
+  return (
+    <header class={styles.header}>
+     🌄 Daily Todo List 🚀
+    </header>
+  );
+}
+
+export default Header;
